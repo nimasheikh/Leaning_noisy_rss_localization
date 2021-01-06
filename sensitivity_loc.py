ee0d41f8482976e6c3cd2f1d983e1c74929b8420
nima <nima.sheikholeslami@mail.utoronto.ca>
2021-01-06 01:36:08 -0500

    considering various portions of radio map as cr data

--- sensitivity_loc.py
+++ sensitivity_loc.py
@@ -11,37 +11,45 @@ if __name__ == "__main__":
     
 
     num_exp = 1000
+    n_cr_data_portion_cases = 11
     Noise_scale = np.linspace(0, 45, 19)
-    
-    d_simple_average = np.zeros([num_exp, len(Noise_scale)])
-    d_ignore_noisy_data = np.zeros([num_exp, len(Noise_scale)])
-    d_perfect_data = np.zeros([num_exp, len(Noise_scale)])
+    length_scale = 5   
+ 
+    d_simple_average = np.zeros([num_exp, n_cr_data_portion_cases, len(Noise_scale)])
+    d_ignore_noisy_data = np.zeros([num_exp, n_cr_data_portion_cases, len(Noise_scale)])
+    d_perfect_data = np.zeros([num_exp, n_cr_data_portion_cases,len(Noise_scale)])
 
 
-    n_test, cr_data_portion = 44, 0.5
-    radio, radio_loc, test, test_loc, cr_idx, cl_idx = split_data(R, L, n_test, cr_data_portion)
-    
 
 
-    length_scale = 5
+    Cr_data_portion = np.linspace(0, 1, n_cr_data_portion_cases)
+    n_test = 44
+
     for i in range(num_exp):
         print(i)
-        for j in range(len(Noise_scale)):
+        for c in range(n_cr_data_portion_cases):    
+            cr_data_portion = Cr_data_portion[c]  
+            radio, radio_loc, test, test_loc, cr_idx, cl_idx = split_data(R, L, n_test, cr_data_portion)
+            
 
-            noise_scale = Noise_scale[j]
-            radio_loc_noisy = add_noise_Loc(radio_loc, cr_idx, noise_scale)
-            
-            loc_hat_test_simple_avg = soft_knn(radio, radio_loc_noisy, test, RBF(length_scale))
-            loc_hat_test_ignore_noisy_data = soft_knn(radio[cl_idx], radio_loc[cl_idx], test)
-            loc_hat_test_perfect_data = soft_knn(radio, radio_loc, test, RBF(length_scale))
-    
-            d_simple_average[i,j] = np.mean(np.linalg.norm(loc_hat_test_simple_avg - test_loc, axis = -1))
-            d_ignore_noisy_data[i,j] = np.mean(np.linalg.norm(loc_hat_test_ignore_noisy_data- test_loc, axis = -1))
-            d_perfect_data[i,j] = np.mean(np.linalg.norm(loc_hat_test_perfect_data - test_loc, axis = -1))
-            
-    
+
+            for j in range(len(Noise_scale)):
+
+                noise_scale = Noise_scale[j]
+                radio_loc_noisy = add_noise_Loc(radio_loc, cr_idx, noise_scale)
+                
+                loc_hat_test_simple_avg = soft_knn(radio, radio_loc_noisy, test, RBF(length_scale))
+                loc_hat_test_ignore_noisy_data = soft_knn(radio[cl_idx], radio_loc[cl_idx], test)
+                loc_hat_test_perfect_data = soft_knn(radio, radio_loc, test, RBF(length_scale))
+        
+                d_simple_average[i,j] = np.mean(np.linalg.norm(loc_hat_test_simple_avg - test_loc, axis = -1))
+                if j == 0:
+                    d_ignore_noisy_data[i,:] = np.mean(np.linalg.norm(loc_hat_test_ignore_noisy_data- test_loc, axis = -1))
+                    d_perfect_data[i,:] = np.mean(np.linalg.norm(loc_hat_test_perfect_data - test_loc, axis = -1))
+                
+        
     D_ = [d_simple_average, d_ignore_noisy_data, d_perfect_data]
-    
+        
     
     
     pass
