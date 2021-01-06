e3db1161c5ca0ca727e07c01e1a2df2e2f6eac73
nima <nima.sheikholeslami@mail.utoronto.ca>
2021-01-06 00:18:41 -0500

    finishing up with sens_loc

--- sensitivity_loc.py
+++ sensitivity_loc.py
@@ -29,18 +29,18 @@ if __name__ == "__main__":
         for j in range(len(Noise_scale)):
 
             noise_scale = Noise_scale[j]
-            radio_loc_noisy = add_noise_Loc(radio, cr_idx, noise_scale)
+            radio_loc_noisy = add_noise_Loc(radio_loc, cr_idx, noise_scale)
             
             loc_hat_test_simple_avg = soft_knn(radio, radio_loc_noisy, test, RBF(length_scale))
             loc_hat_test_ignore_noisy_data = soft_knn(radio[cl_idx], radio_loc[cl_idx], test)
-            loc_hat_test_perfect_data = soft_knn(radio, radio_loc_noisy, test, RBF(length_scale))
+            loc_hat_test_perfect_data = soft_knn(radio, radio_loc, test, RBF(length_scale))
     
             d_simple_average[i,j] = np.mean(np.linalg.norm(loc_hat_test_simple_avg - test_loc, axis = -1))
             d_ignore_noisy_data[i,j] = np.mean(np.linalg.norm(loc_hat_test_ignore_noisy_data- test_loc, axis = -1))
             d_perfect_data[i,j] = np.mean(np.linalg.norm(loc_hat_test_perfect_data - test_loc, axis = -1))
             
     
-    D = [d_simple_average, d_ignore_noisy_data, d_perfect_data]
+    D_ = [d_simple_average, d_ignore_noisy_data, d_perfect_data]
     
     
     
