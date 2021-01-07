3fe546a4d59b4ac6b8503c8ead7635ab4aa923b5
server <me@email.com>
2021-01-07 01:37:15 -0500

    fixing bugs on saving data

--- sensitivity_loc.py
+++ sensitivity_loc.py
@@ -20,8 +20,8 @@ if __name__ == "__main__":
  
     d_simple_average = np.zeros([num_exp, n_cr_data_portion_cases, len(Noise_scale)])
     d_ignore_noisy_data = np.zeros([num_exp, n_cr_data_portion_cases, len(Noise_scale)])
-    d_perfect_data = np.zeros([num_exp, n_cr_data_portion_cases,len(Noise_scale)])
-
+    d_perfect_data = np.zeros([num_exp, n_cr_data_portion_cases, len(Noise_scale)])
+    d_train_model = np.zeros([num_exp, n_cr_data_portion_cases, len(Noise_scale)])
 
 
 
@@ -44,19 +44,21 @@ if __name__ == "__main__":
                 noise_scale = Noise_scale[j]
                 radio_loc_noisy = add_noise_Loc(radio_loc, cr_idx, noise_scale)
 
-                matrix_model = train_model(radio, radio_loc, val_data, val_loc)
+                matrix_model = train_model(radio, radio_loc_noisy, val_data, val_loc)
                 
                 loc_hat_test_simple_avg = soft_knn(radio, radio_loc_noisy, test, RBF(length_scale))
                 loc_hat_test_ignore_noisy_data = soft_knn(radio[cl_idx], radio_loc[cl_idx], test, RBF(length_scale))
                 loc_hat_test_perfect_data = soft_knn(radio, radio_loc, test, RBF(length_scale))
-        
+                loc_hat_test_train_model = matrix_model(test).detach()
+	
                 d_simple_average[i,c,j] = np.mean(np.linalg.norm(loc_hat_test_simple_avg - test_loc, axis = -1))
+                d_train_model[i,c,j] = np.mean(np.linalg.norm(loc_hat_test_train_model - test_loc, axsi = -1))
                 if j == 0:
                     d_ignore_noisy_data[i,c,:] = np.mean(np.linalg.norm(loc_hat_test_ignore_noisy_data- test_loc, axis = -1))
                     d_perfect_data[i,c,:] = np.mean(np.linalg.norm(loc_hat_test_perfect_data - test_loc, axis = -1))
                 
         
-    D_ = [d_simple_average, d_ignore_noisy_data, d_perfect_data]
+    D_ = [d_simple_average, d_train_model, d_ignore_noisy_data, d_perfect_data]
         
     
     np.save('result/Loc_sensitivity_01_07', D_)
